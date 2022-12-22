e0a39192e923d5d1ff32777ca3d9af6d94e7d1a4
Aleksandr Artemenkov <alartum@gmail.com>
2022-12-22 19:58:18 +0200

    Sample package

---
+++ sample_module/sample_module.py
@@ -0,0 +1,24 @@
+class SampleClass:
+    def __init__(self, param1: int, param2: str) -> None:
+        """Creates a new SampleClass object.
+
+        Parameters
+        ----------
+        param1 : int
+            First parameter.
+        param2 : str
+            Second parameter.
+        """
+        self.param1 = param1
+        self.param2 = param2
+
+    def print_hello(self, who: str = "World"):
+        """Prints `Hello, <someone>!` where <someone> is
+        specified in the parameters.
+
+        Parameters
+        ----------
+        who : str, optional
+            Something to say hello to, by default "World"
+        """
+        print(f"Hello, {who}!")
